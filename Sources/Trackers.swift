a53ce8a71179ef69448b0ed6e4ab80d445f19e7a
Khoa Pham <onmyway133@gmail.com>
2017-06-09 15:05:00 +0200

    Track viewDidAppear

--- Sources/Trackers.swift
+++ Sources/Trackers.swift
@@ -1,5 +1,28 @@
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
 
-public func track<T: NSObject>(kClass: T.Type, block: (T) -> Void) {
+public func track<T: UIViewController>(type: T.Type, block: (T) -> Void) {
+  let original = #selector(UIViewController.viewDidAppear(_:))
+  let swizled = #selector(UIViewController.trackers_viewDidAppear(_:))
+  swizzle(type: type, originalSelector: original, swizzledSelector: swizled)
+}
 
+extension UIViewController {
+  func trackers_viewDidAppear(_ animated: Bool) {
+    trackers_viewDidAppear(animated)
+  }
+}
+
+func swizzle<T: UIViewController>(type: T.Type, originalSelector: Selector, swizzledSelector: Selector) {
+  let originalMethod = class_getInstanceMethod(type, originalSelector)
+  let swizzledMethod = class_getInstanceMethod(type, swizzledSelector)
+
+  let didAddMethod = class_addMethod(type, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+
+  if didAddMethod {
+    class_replaceMethod(type, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+  } else {
+    method_exchangeImplementations(originalMethod, swizzledMethod)
+  }
 }
